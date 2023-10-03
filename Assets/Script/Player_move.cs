5a011d60b0db759232e8efb32358f541ffec7937
daymoove <daymoove@gmail.com>
2023-10-03 14:54:18 +0200

    PlayerMove + Terrain

---
+++ Assets/Script/Player_move.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player_move : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    Vector3 movementVector;
+    [SerializeField]float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+        movementVector *= speed;
+
+        rigidbody2d.velocity = movementVector;
+    }
+}
